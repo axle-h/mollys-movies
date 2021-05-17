b9aad133ce8fbba27d0dc759ee05d073e2d254f6
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-05-17 11:49:02 +0100

    update angular

--- ui/src/app/movies/components/poster/poster.component.spec.ts
+++ ui/src/app/movies/components/poster/poster.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PosterComponent } from './poster.component';
 
@@ -6,7 +6,7 @@ describe('PosterComponent', () => {
   let component: PosterComponent;
   let fixture: ComponentFixture<PosterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PosterComponent ]
     })
